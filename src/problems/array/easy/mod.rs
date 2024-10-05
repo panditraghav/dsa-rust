6e52f5c3458813ace9e30284efe96215271257ef
Raghav Pandit <panditraghav18@gmail.com>
2024-10-05 14:07:58 +0530

    Added better approach for missing number problem

--- src/problems/array/easy/mod.rs
+++ src/problems/array/easy/mod.rs
@@ -2,7 +2,7 @@
 /// Find the number(between 1 to N), that is not present in the given array.
 pub mod missing_element_in_array {
     use crate::utils::input::get_input_vector;
-    use std::io::stdin;
+    use std::{collections::HashMap, io::stdin};
 
     fn take_input() -> (Vec<i32>, u8) {
         println!("Enter number of elements: ");
@@ -12,7 +12,7 @@ pub mod missing_element_in_array {
 
         (get_input_vector(u32::from(num_int - 1)), num_int)
     }
-
+    /// Using nested loop
     pub fn brute() {
         println!("missing_element_in_array: brute");
         let (arr, size) = take_input();
@@ -30,5 +30,32 @@ pub mod missing_element_in_array {
                 break;
             }
         }
+    }
+
+    // Usin hash map
+    pub fn better() {
+        println!("missing_element_in_array: better");
+
+        let (arr, size) = take_input();
+        println!("Array is:- {arr:?}");
+
+        let mut map: HashMap<i32, u8> = HashMap::new();
+
+        for i in arr {
+            map.insert(i, 1);
+        }
+
+        let mut not_found: u8 = 0;
+        for i in 1..size {
+            let val = map.get(&i32::from(i));
+            match val {
+                None => {
+                    not_found = i;
+                    break;
+                }
+                Some(_) => (),
+            };
+        }
+        println!("The missing number is {not_found}");
     }
 }
